e5a65cb453d0fc7a698b201dd1156a20c02a77b6
wanyue <2660321030@qq.com>
2016-06-01 18:04:28 +0800

    The first submission
    第一次提交guitar2代码

---
+++ GuitarSystem2/src/eb/java/guitar/DBUtil.java
@@ -0,0 +1,34 @@
+﻿package eb.java.guitar;
+
+import java.sql.*;
+
+public class DBUtil {
+	private static String driverName;
+	private static String dbURL;
+	static{
+		driverName = "org.sqlite.JDBC";
+		dbURL="jdbc:sqlite:\\D:/workspace/GuitarSystem/WebContent/database/GuitarData2.db";
+	}
+	public static Connection open(){
+		try {
+				Class.forName(driverName);
+			try {
+				return DriverManager.getConnection(dbURL);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+			} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	public static void close(Connection Conn){
+		if(Conn != null){
+			try {
+				Conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
